e9c2b7166fb24d22e1540f488a96820edddcbeab
Zack Michener <zjmichen@unca.edu>
2011-10-07 14:42:36 -0400

    placed program under GNU GPL, added BUGS and LICENSE files

--- window.c
+++ window.c
@@ -1,7 +1,22 @@
-/* window.c
- * This file handles general window operations such as exposure, icon loading,
- * getting cursor position, child windows, etc.
- */
+/****************************************************************************\
+* Copyright 2011 Zack Michener <zjmichen@unca.edu>                           *
+*                                                                            *
+* This file is part of zRuler.                                               *
+*                                                                            *
+* zRuler is free software; you can redistribute it and/or modify             *
+* it under the terms of the GNU General Public License as published by       *
+* the Free Software Foundation; either version 3 of the License, or          *
+* (at your option) any later version.                                        *
+*                                                                            *
+* zRuler is distributed in the hope that it will be useful,                  *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of             *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the              *
+* GNU Library General Public License for more details.                       *
+*                                                                            *
+* You should have received a copy of the GNU General Public License          *
+* along with zRuler; if not, write to the Free Software                      *
+* Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA. *
+\****************************************************************************/
 
 #include <X11/Xlib.h>
 #include <gtk/gtk.h>
